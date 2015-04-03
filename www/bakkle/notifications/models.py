cae9b33eb5e4047876704f1f8f1f0355bf8a53e4
watterlm <watterlm@rose-hulman.edu>
2015-04-03 10:31:11 -0400

    Fixed errors in items and notification models. Added beginnings of account model. Updated gitignore to ignor .pyc
    Former-commit-id: b316b648ab212f6fcfdebe240a45289a48006dd8

--- www/bakkle/notifications/models.py
+++ www/bakkle/notifications/models.py
@@ -20,23 +20,22 @@ if use_sandbox:
 
 
 class PushRegistrations(models.Model):
-        user_id = models.CharField(max_length=200)
-	device_token = models.CharField(max_length=200)
-	subscribe_date = models.DateTimeField('date subscribed')
-	enabled = models.IntegerField(default=1)
-	def __str__(self):
-		return "User: {id}".format(id=self.user_id)
+    user_id = models.CharField(max_length=200)
+    device_token = models.CharField(max_length=200)
+    subscribe_date = models.DateTimeField('date subscribed')
+    enabled = models.IntegerField(default=1)
+    def __str__(self):
+        return "User: {id}".format(id=self.user_id)
 
-	def isActive(self):
-		return self.enabled==1
+    def isActive(self):
+        return self.enabled==1
 
-
-        def send_notification(self, message, sound="default", badge=0):
-                print(cert_file)
-                apns = APNs(True, cert_file=cert_file, key_file=key_file)
-                payload = Payload(alert='bob', sound='default', badge='0')
-                dt = self.device_token.replace(' ', '').replace('<', '').replace('>', '')
-                print("notifying {} token {}".format(self.user_id, dt))
-                apns.gateway_server.send_notification(dt, payload)
-                #TODO: Log this to db so we know what we did.
+    def send_notification(self, message, sound="default", badge=0):
+        print(cert_file)
+        apns = APNs(True, cert_file=cert_file, key_file=key_file)
+        payload = Payload(alert='bob', sound='default', badge='0')
+        dt = self.device_token.replace(' ', '').replace('<', '').replace('>', '')
+        print("notifying {} token {}".format(self.user_id, dt))
+        apns.gateway_server.send_notification(dt, payload)
+        #TODO: Log this to db so we know what we did.
 
