25d59a79e68cc156edcf3e17c1339b93eb0bb9b9
Sandor Pethes <git@pethes.com>
2015-04-06 17:05:08 -0400

    Added slash to push register.
    Former-commit-id: 989261725e0669b3506848ca3ff3392f3703fd0f

--- www/bakkle/account/urls.py
+++ www/bakkle/account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^facebook/$', views.facebook, name='facebook'),
     url(r'^(?P<account_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^device/(?P<device_id>[0-9]+)/$', views.device_detail, name='device_detail'),
-    url(r'^device/register_push$', views.device_register_push, name='device_register_push'),
+    url(r'^device/register_push/$', views.device_register_push, name='device_register_push'),
     url(r'^device/(?P<device_id>[0-9]+)/notify/$', views.device_notify, name='device_notify'),
 ]
