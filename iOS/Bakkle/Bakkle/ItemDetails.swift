17dd970efda3ab60f1162834a4afaf2993ac9c90
Ishank Tandon <tandoni@rose-hulman.edu>
2015-04-27 14:45:08 -0400

    update feed
    Former-commit-id: 1dda18d97d43ed64ff169950e69a6b25473f195b

--- iOS/Bakkle/Bakkle/ItemDetails.swift
+++ iOS/Bakkle/Bakkle/ItemDetails.swift
@@ -12,11 +12,35 @@ class ItemDetails: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.whiteColor()
+        activityInd.startAnimating()
 
-        // Do any additional setup after loading the view.
     }
-    @IBOutlet weak var imgDet: UIImageView!
+    
+    override func viewWillAppear(animated: Bool) {
+        var topItem = Bakkle.sharedInstance.feedItems[0]
+        var itemDetails: NSDictionary = topItem.valueForKey("fields") as! NSDictionary
+        let imgURLs: String = itemDetails.valueForKey("image_urls") as! String
+        
+        let imgURL = NSURL(string: imgURLs)
+        if let imgData = NSData(contentsOfURL: imgURL!) {
+            dispatch_async(dispatch_get_main_queue()) {
+                println("[FeedScreen] displaying image (top)")
+                self.imgDet.image = UIImage(data: imgData)
+                self.imgDet.contentMode = UIViewContentMode.ScaleAspectFill
+            }
+        }
 
+    }
+    
+    @IBAction func goback(sender: AnyObject) {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    @IBOutlet weak var imgDet: UIImageView!
+    @IBOutlet weak var activityInd: UIActivityIndicatorView!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
