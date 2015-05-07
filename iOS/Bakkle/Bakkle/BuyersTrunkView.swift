03cffc9889ceb95a21edfde85221acdcaabae8d5
Sandor Pethes <git@pethes.com>
2015-05-06 23:05:12 -0400

    Fixed lazy load of table images. Added notification for trunk updates.
    Former-commit-id: f7963b9c8ac3cfd1ef49e2e29f6b4daddef63594

--- iOS/Bakkle/Bakkle/BuyersTrunkView.swift
+++ iOS/Bakkle/Bakkle/BuyersTrunkView.swift
@@ -16,7 +16,7 @@ class BuyersTrunkCell : UITableViewCell {
     @IBOutlet var tagLabel: UILabel?
     @IBOutlet var distanceLabel: UILabel?
     
-    func loadCell(imgURLs: [String], title: String, price: String, delivery: String, tags: [String]) {
+    func loadCell(imgURLs: [String], title: String, price: String, delivery: String, tags: [String], indexPath: NSIndexPath) {
         println("[BuyersTrunk] Attempting to load image in cell")
         dispatch_async(dispatch_get_global_queue(
             Int(QOS_CLASS_USER_INTERACTIVE.value), 0)) {
@@ -24,11 +24,14 @@ class BuyersTrunkCell : UITableViewCell {
                 let imgURL = NSURL(string: firstURL)
                 if let imgData = NSData(contentsOfURL: imgURL!) {
                     dispatch_async(dispatch_get_main_queue()) {
-                        println("[BuyersTrunk] displaying cell image")
-                        self.itemImage!.image = UIImage(data: imgData)
-                        self.itemImage!.contentMode = UIViewContentMode.ScaleAspectFill
-                        self.itemImage!.layer.cornerRadius = 4.0
-                        self.itemImage!.clipsToBounds = true
+                        let superview: UITableView = self.superview?.superview! as! UITableView
+                        if let cellToUpdate = superview.cellForRowAtIndexPath(indexPath) {
+                            println("[BuyersTrunk] displaying cell image")
+                            self.itemImage!.image = UIImage(data: imgData)
+                            self.itemImage?.contentMode = UIViewContentMode.ScaleAspectFill
+                            self.itemImage?.layer.cornerRadius = 4.0
+                            self.itemImage?.clipsToBounds = true
+                        }
                     }
                 }
         }
@@ -51,10 +54,14 @@ class BuyersTrunkView: UITableViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
-        Bakkle.sharedInstance.populateTrunk({
-//            updateUI()
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        let mainQueue = NSOperationQueue.mainQueue()
+        var observer = notificationCenter.addObserverForName(Bakkle.bkTrunkUpdate, object: nil, queue: mainQueue) { _ in
             self.tableView.reloadData()
-        });
+        }
+        
+        Bakkle.sharedInstance.populateTrunk({});
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -73,6 +80,10 @@ class BuyersTrunkView: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         println("[BuyersTrunk] Updating table view")
         let cell = self.tableView.dequeueReusableCellWithIdentifier("BuyersTrunkCell") as! BuyersTrunkCell
+        cell.itemImage?.image = UIImage(named: "blank.png")
+        cell.itemImage?.contentMode = UIViewContentMode.ScaleAspectFill
+        cell.itemImage?.layer.cornerRadius = 4.0
+        cell.itemImage?.clipsToBounds = true
         if Bakkle.sharedInstance.trunkItems.count > 0 {
             let item : NSDictionary = Bakkle.sharedInstance.trunkItems[indexPath.row] as! NSDictionary
 //            if let x: AnyObject = topItem.valueForKey("pk") {
@@ -85,7 +96,7 @@ class BuyersTrunkView: UITableViewController {
             let price : String = item.valueForKey("price") as! String
             let delivery : String = item.valueForKey("method") as! String
             let tags : [String] = item.valueForKey("tags") as! [String]
-            cell.loadCell(imgURLs, title: title, price: price, delivery: delivery, tags: tags)
+            cell.loadCell(imgURLs, title: title, price: price, delivery: delivery, tags: tags, indexPath: indexPath)
         } else {
             // No items in trunk
             println("[BuyersTrunk] Tried loading trunk items, none to be found")
