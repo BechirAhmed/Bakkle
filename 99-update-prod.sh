5578787ce0e2c7f4b1441662818141737ca4015a
root <root@bakkle.rhventures.org>
2015-05-12 09:40:17 -0900

    made server install more generic.
    Former-commit-id: eb7e38b999b0ea76d877f85c32bf247334bea4dc

--- 99-update-prod.sh
+++ 99-update-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /home/ubuntu/omnisite-bakkle
-sudo ssh-agent bash -c 'ssh-add /etc/ext-bakkle.key; git pull'
+#cd /home/ubuntu/omnisite-bakkle
+sudo ssh-agent bash -c 'ssh-add ./ext-bakkle.key; git pull'
 sudo ./5-update.sh
 
