ec655ee60c5389688bb668ff978c66e016b15b5f
root <git@pethes.com>
2015-05-13 12:04:13 -0900

    updated update scripts
    Former-commit-id: 580a7069d20e012e68a6a609aa8c5ee0c2a62c81

--- 2-webserver.sh
+++ 2-webserver.sh
@@ -7,12 +7,16 @@ sudo install -m 644 etc/ssl/com.bakkle/com.bakkle.key        /etc/ssl/com.bakkle
 sudo install -m 644 etc/ssl/com.bakkle/com.bakkle.pem        /etc/ssl/com.bakkle/
 
 sudo install -m 644 etc/nginx/sites-available/com.bakkle.app /etc/nginx/sites-available/
+sudo install -m 644 etc/nginx/sites-available/org.rhventures.bakkle /etc/nginx/sites-available/
 
 sudo apt-get install nginx
 
 sudo ./bin/nginx_dissite 000-default
 if [ `hostname` == 'ip-172-31-21-18' ]; then
    sudo ./bin/nginx_ensite com.bakkle.app
+fi
+if [ `hostname` == 'bakkle' ]; then
+   sudo ./bin/nginx_ensite org.rhventures.bakkle
 fi
 
 sudo service nginx restart
