7508b8dec6b5a81e588a4147e466732924383eb0
Jonathan Earles <earlesja@rose-hulman.edu>
2015-05-13 18:14:19 -0400

    permission issues with bkbuild
    Former-commit-id: 2c365a35a21bf05b4a52337b456e4ef214ccfd18

--- server-test-build.sh
+++ server-test-build.sh
@@ -10,9 +10,7 @@ user=bkbuild
 # Auth key for remote access
 key=${DIR}/keys/bkbuild
 # Root directory of web modules on remote machine
-webRoot=/bakkle
-# Location of database scripts on remote machine
-scripts=/bakkle
+webRoot=/omnisite-bakkle
 
 # Root directory for website file backups
 backups=/backups
@@ -21,10 +19,10 @@ sshCommand="ssh $address -l bkbuild -i $key"
 scpCommand="scp -r -i $key"
 
 echo "### Copy Files to Test Server"
-#eval sudo $scpCommand $key ${DIR} $user@$address:$webRoot
+# eval sudo $scpCommand ${DIR} $user@$address:/
 eval $sshCommand sudo ssh-agent bash -c 'ssh-add ./ext-bakkle.key; git pull'
-eval $sshCommand $webRoot/omnisite-bakkle/1-system-deps.sh
-eval $sshCommand $webRoot/omnisite-bakkle/2-webserver.sh
-eval $sshCommand $webRoot/omnisite-bakkle/3-webapp.deps.sh
-eval $sshCommand $webRoot/omnisite-bakkle/5-update.sh
+eval $sshCommand $webRoot/1-system-deps.sh
+eval $sshCommand $webRoot/2-webserver.sh
+eval $sshCommand $webRoot/3-webapp.deps.sh
+eval $sshCommand $webRoot/5-update.sh
 echo "### End Test";
