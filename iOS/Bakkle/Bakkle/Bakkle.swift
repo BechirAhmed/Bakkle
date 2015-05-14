70cc543422708c7181dda7f97c364dd973c27f5b
Sandor Pethes <git@pethes.com>
2015-05-14 15:05:23 -0400

    Typo underscore.
    Former-commit-id: 77fe78849a4537a32a3c04ba4e19060d648e3ed8

--- iOS/Bakkle/Bakkle/Bakkle.swift
+++ iOS/Bakkle/Bakkle/Bakkle.swift
@@ -234,7 +234,7 @@ class Bakkle : NSObject, CLLocationManagerDelegate {
             let encLocation = user_location.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
         
             request.HTTPMethod = "POST"
-            let postString = "device_uuid=\(self.deviceUUID)&user_id=\(self.facebook_id_str)&screen_width=\(screen_width)&screen_height=\(screen_height)&app_version=\(a)&app_build=\(b)&user_location_\(encLocation)"
+            let postString = "device_uuid=\(self.deviceUUID)&user_id=\(self.facebook_id_str)&screen_width=\(screen_width)&screen_height=\(screen_height)&app_version=\(a)&app_build=\(b)&user_location=\(encLocation)"
             request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
             
             println("[Bakkle] login (facebook)")
