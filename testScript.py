d145e7fc687217cc69c2780ec8af767dffb4e510
Jonathan Earles <earlesja@rose-hulman.edu>
2015-05-20 15:59:59 -0400

    Lights off - then on
    Former-commit-id: c9538428024dc58a7b0bd5c1889407dc21078d00

--- testScript.py
+++ testScript.py
@@ -27,11 +27,15 @@ if buildStatus.has_key("health"):
     if buildStatus["health"] == 1:
         req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A1/OFF")
         urllib2.urlopen(req)
+        req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A2/OFF")
+        urllib2.urlopen(req)
         req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A2/ON")
         urllib2.urlopen(req)
         print "Website is Healthy! Green GO! Red NO!"
     else:
         req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A2/OFF")
+        urllib2.urlopen(req)
+        req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A1/OFF")
         urllib2.urlopen(req)
         req = urllib2.Request("http://sauron.rhventures.org:8765/lamp/A1/ON")
         urllib2.urlopen(req)
