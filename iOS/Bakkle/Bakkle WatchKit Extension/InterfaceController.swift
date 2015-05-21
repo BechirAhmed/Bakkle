ada8dade20128a8465d99ebe3abfaa6bdbdbd001
Watterson, Lindsey M <watterlm@rose-hulman.edu>
2015-05-21 13:36:55 -0400

    Added message for when not logged in to Bakkle and dictionary is null
    Former-commit-id: b0e1078a8c8367a3638980cf7999bde529b55243

--- iOS/Bakkle/Bakkle WatchKit Extension/InterfaceController.swift
+++ iOS/Bakkle/Bakkle WatchKit Extension/InterfaceController.swift
@@ -15,6 +15,9 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var lblItemTitle: WKInterfaceLabel!
     @IBOutlet weak var lblItemPrice: WKInterfaceLabel!
     @IBOutlet weak var lblNoItems: WKInterfaceLabel!
+    @IBOutlet weak var lblRunApp: WKInterfaceLabel!
+    @IBOutlet weak var btnWant: WKInterfaceButton!
+    @IBOutlet weak var btnMeh: WKInterfaceButton!
     
     var item_id: String = ""
     
@@ -31,29 +34,37 @@ class InterfaceController: WKInterfaceController {
         
         WKInterfaceController.openParentApplication(dictonary as! [NSObject : AnyObject], reply: { (replyInfo, error) -> Void in
             
-            var dictionary = replyInfo as NSDictionary
-            
-            let successString = dictionary["success"] as! String
-            if successString == "yes"{
-                self.lblNoItems.setHidden(true)
-                self.lblItemPrice.setHidden(false)
-                self.lblItemTitle.setHidden(false)
-                let item_title = dictionary["item_title"] as! String
-                let item_price = dictionary["item_price"] as! String
-                self.item_id = dictionary["item_id"] as! String
+            if replyInfo != nil{
+                var dictionary = replyInfo as NSDictionary
+                self.lblRunApp.setHidden(true)
+                self.btnMeh.setHidden(false)
+                self.btnWant.setHidden(false)
+                let successString = dictionary["success"] as! String
+                if successString == "yes"{
+                    self.lblNoItems.setHidden(true)
+                    self.lblItemPrice.setHidden(false)
+                    self.lblItemTitle.setHidden(false)
+                    let item_title = dictionary["item_title"] as! String
+                    let item_price = dictionary["item_price"] as! String
+                    self.item_id = dictionary["item_id"] as! String
+                    
+                    
+                    self.lblItemTitle.setText(item_title)
+                    self.lblItemPrice.setText("$" + item_price)
+                    //let image = dictionary["image"] as! UIImage
+                } else {
+                    self.lblNoItems.setHidden(false)
+                    self.lblItemPrice.setHidden(true)
+                    self.lblItemTitle.setHidden(true)
+                    self.item_id = ""
+                }
                 
-                
-                self.lblItemTitle.setText(item_title)
-                self.lblItemPrice.setText("$" + item_price)
-                //let image = dictionary["image"] as! UIImage
-            } else {
-                self.lblNoItems.setHidden(false)
-                self.lblItemPrice.setHidden(true)
-                self.lblItemTitle.setHidden(true)
-                self.item_id = ""
+                println(successString)
+            }else {
+                self.lblRunApp.setHidden(false)
+                self.btnMeh.setHidden(true)
+                self.btnWant.setHidden(true)
             }
-            
-            println(successString)
         })
     }
 
@@ -68,30 +79,38 @@ class InterfaceController: WKInterfaceController {
         
         WKInterfaceController.openParentApplication(dictonary as! [NSObject : AnyObject], reply: { (replyInfo, error) -> Void in
             
-            var dictionary = replyInfo as NSDictionary
-            
-            let successString = dictionary["success"] as! String
-            if successString == "yes"{
-                self.lblNoItems.setHidden(true)
-                self.lblItemPrice.setHidden(false)
-                self.lblItemTitle.setHidden(false)
-                let item_title = dictionary["item_title"] as! String
-                let item_price = dictionary["item_price"] as! String
-                self.item_id = dictionary["item_id"] as! String
-                
-                
-                self.lblItemTitle.setText(item_title)
-                self.lblItemPrice.setText("$" + item_price)
-                //let image = dictionary["image"] as! UIImage
-                
-                
-            } else {
-                self.lblNoItems.setHidden(false)
-                self.lblItemPrice.setHidden(true)
-                self.lblItemTitle.setHidden(true)
-                self.item_id = ""
+            if replyInfo != nil{
+                var dictionary = replyInfo as NSDictionary
+                self.lblRunApp.setHidden(true)
+                self.btnMeh.setHidden(false)
+                self.btnWant.setHidden(false)
+                let successString = dictionary["success"] as! String
+                if successString == "yes"{
+                    self.lblNoItems.setHidden(true)
+                    self.lblItemPrice.setHidden(false)
+                    self.lblItemTitle.setHidden(false)
+                    let item_title = dictionary["item_title"] as! String
+                    let item_price = dictionary["item_price"] as! String
+                    self.item_id = dictionary["item_id"] as! String
+                    
+                    
+                    self.lblItemTitle.setText(item_title)
+                    self.lblItemPrice.setText("$" + item_price)
+                    //let image = dictionary["image"] as! UIImage
+                    
+                    
+                } else {
+                    self.lblNoItems.setHidden(false)
+                    self.lblItemPrice.setHidden(true)
+                    self.lblItemTitle.setHidden(true)
+                    self.item_id = ""
+                }
+                println(successString)
+            }else {
+                self.lblRunApp.setHidden(false)
+                self.btnMeh.setHidden(true)
+                self.btnWant.setHidden(true)
             }
-            println(successString)
         })
     }
     
@@ -100,30 +119,39 @@ class InterfaceController: WKInterfaceController {
         var dictonary = NSDictionary(objects: ["want",item_id], forKeys: ["type", "item_id"])
         WKInterfaceController.openParentApplication(dictonary as! [NSObject : AnyObject], reply: { (replyInfo, error) -> Void in
             
-            var dictionary = replyInfo as NSDictionary
+            if replyInfo != nil{
+                var dictionary = replyInfo as NSDictionary
+                self.lblRunApp.setHidden(true)
+                self.btnMeh.setHidden(false)
+                self.btnWant.setHidden(false)
             
-            let successString = dictionary["success"] as! String
-            if successString == "yes"{
-                self.lblNoItems.setHidden(true)
-                self.lblItemPrice.setHidden(false)
-                self.lblItemTitle.setHidden(false)
-                let item_title = dictionary["item_title"] as! String
-                let item_price = dictionary["item_price"] as! String
-                self.item_id = dictionary["item_id"] as! String
-                
-                
-                self.lblItemTitle.setText(item_title)
-                self.lblItemPrice.setText("$" + item_price)
-                //let image = dictionary["image"] as! UIImage
-                
-                
-            } else {
-                self.lblNoItems.setHidden(false)
-                self.lblItemPrice.setHidden(true)
-                self.lblItemTitle.setHidden(true)
-                self.item_id = ""
+                let successString = dictionary["success"] as! String
+                if successString == "yes"{
+                    self.lblNoItems.setHidden(true)
+                    self.lblItemPrice.setHidden(false)
+                    self.lblItemTitle.setHidden(false)
+                    let item_title = dictionary["item_title"] as! String
+                    let item_price = dictionary["item_price"] as! String
+                    self.item_id = dictionary["item_id"] as! String
+                    
+                    
+                    self.lblItemTitle.setText(item_title)
+                    self.lblItemPrice.setText("$" + item_price)
+                    //let image = dictionary["image"] as! UIImage
+                    
+                    
+                } else {
+                    self.lblNoItems.setHidden(false)
+                    self.lblItemPrice.setHidden(true)
+                    self.lblItemTitle.setHidden(true)
+                    self.item_id = ""
+                }
+                println(successString)
+            }else {
+                self.lblRunApp.setHidden(false)
+                self.btnMeh.setHidden(true)
+                self.btnWant.setHidden(true)
             }
-            println(successString)
         })
     }
 }
