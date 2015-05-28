ca4376795b40915e395b21492097988a357fead4
watterlm <watterlm@rose-hulman.edu>
2015-05-28 10:15:52 -0400

    Updates for movement.
    Former-commit-id: cc9c53322894494c254e61b5affaa1d094317959

--- Android/Bakkle/app/src/main/java/com/bakkle/bakkle/FeedFragment.java
+++ Android/Bakkle/app/src/main/java/com/bakkle/bakkle/FeedFragment.java
@@ -4,16 +4,23 @@ package com.bakkle.bakkle;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements View.OnTouchListener {
 
+    private ImageView card;
+    private ViewGroup mRrootLayout;
+    private int _xDelta;
+    private int _yDelta;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -24,8 +31,41 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        mRrootLayout = (ViewGroup) view;
+        card = (ImageView) view.findViewById(R.id.card);
+        card.setOnTouchListener(this);
+
         return view;
     }
 
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int X = (int) event.getRawX();
+        final int Y = (int) event.getRawY();
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) v.getLayoutParams();
+                _xDelta = X - lParams.leftMargin;
+                _yDelta = Y - lParams.topMargin;
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            case MotionEvent.ACTION_POINTER_DOWN:
+                break;
+            case MotionEvent.ACTION_POINTER_UP:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) v
+                        .getLayoutParams();
+                layoutParams.leftMargin = X - _xDelta;
+                layoutParams.topMargin = Y - _yDelta;
+                layoutParams.rightMargin = -250;
+                layoutParams.bottomMargin = -250;
+                v.setLayoutParams(layoutParams);
+                break;
+        }
+        mRrootLayout.invalidate();
+        return true;
+    }
 }
--- Android/Bakkle/app/src/main/java/com/bakkle/bakkle/SplashActivity.java
+++ Android/Bakkle/app/src/main/java/com/bakkle/bakkle/SplashActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -19,6 +25,23 @@ public class SplashActivity extends Activity {
         getActionBar().hide();
         setContentView(R.layout.activity_splash);
         mContext = this;
+
+// TODO: Use this to get the Facebook Development hash for each new computer. The hash will appear in logcat. Send the hash to Rameen
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.bakkle.bakkle",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
 
         new Handler().postDelayed(new Runnable() {
 
