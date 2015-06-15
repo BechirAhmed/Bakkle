e7b01cdcbb417485464e9311ff32a3d9233da4aa
Wong, Benedict S <wongb@rose-hulman.edu>
2015-06-15 12:28:53 -0400

    Added comments to demo
    Former-commit-id: 08c67e63e6a944d2cbd29d756d34e76e3bed6f10

--- iOS/Bakkle/Bakkle/ViewController.m
+++ iOS/Bakkle/Bakkle/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 #import "WSManager.h"
-#import "WSTest-Swift.h"
+#import "Bakkle-Swift.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *chatHistory;
@@ -20,6 +20,7 @@
 @implementation ViewController
 
 - (IBAction)chatSendBtn:(id)sender {
+    //send a chat.
     WSRequest *request = [[WSSendChatMessageRequest alloc]
                           initWithChatId:self.chatId
                           message:self.chatInput.text];
@@ -33,10 +34,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    //must set authentication and autoregistration before connecting.
     [WSManager setAuthenticationWithUUID:@"81FEEEDD-C99C-4E50-B671-4302F146441B" withToken:@"4c708bda45351147d32b5c3f541b76ba_3"];
     [WSManager setAutoRegister:true];
     [WSManager connectWS];
     
+    //register any handlers here for receipt of a new chat message, etc.
     [WSManager registerMessageHandler:^void(NSDictionary* messageData){
         NSLog(@"[OnMessageHandler] OnMessageHandler received new message: '%@' from: %@", [messageData objectForKey:@"message"], [messageData objectForKey:@"messageOrigin"]);
         
@@ -48,7 +51,7 @@
         [self.chatHistory scrollRangeToVisible:NSMakeRange(self.chatHistory.contentSize.height, 0)];
     } forNotification:@"newMessage"];
     
-    
+    //get chat id by creating a new chat.
     WSRequest *request = [[WSStartChatRequest alloc] initWithItemId:@"12"];
     request.successHandler = ^void(NSDictionary* dict){
         
