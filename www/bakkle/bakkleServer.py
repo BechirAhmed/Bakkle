6274ef552515a58609ec4f2529f242aa9ca70e3b
Wong, Benedict S <wongb@rose-hulman.edu>
2015-06-16 18:00:05 -0400

    Revert "Added logic to handle dynamic DB"
    This reverts commit 08553212a1b190d27fbebec13a841f02ca39661e [formerly 32b1614ee4f5bfde99ff00740642388acacbad01].
    
    
    Former-commit-id: 2300984b454687b5dc44331451e86d0db13098a8

--- www/bakkle/bakkleServer.py
+++ www/bakkle/bakkleServer.py
@@ -13,42 +13,19 @@ from django.conf import settings
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DATABASES = {
-    'dev': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     },
-     'testdb': {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'testdb',
         'USER': 'root',
         'PASSWORD': 'Bakkle123',
         'HOST': 'bakkle.cw8vja43bda8.us-west-2.rds.amazonaws.com',
         'PORT': '5432',
-     },
-     'production': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bakkle',
-        'USER': 'root',
-        'PASSWORD': 'Bakkle123',
-        'HOST': 'bakkle.cw8vja43bda8.us-west-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
+        },
 }
-import socket
-hostname = socket.gethostname()
-
-if(hostname == 'ip-172-31.21.118' or hostname == 'ip-172-31-27-192'):
-    DATABASES['default'] = DATABASES['production']
-    print("Using database production");
-
-elif(hostname == 'bakkle'):
-    DATABASES['default'] = DATABASES['testdb']
-    print("Using database testdb");
-
-elif(hostname == 'rhv-bakkle-bld' or hostname == 'RHV-291SCS-Linux'):
-    DATABASES['default'] = DATABASES['dev']
-    print("Using database dev");
-
 settings.configure(DATABASES=DATABASES)
 
 
