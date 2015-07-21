8c54f177cc3194f5a2a94d6c5e53c8132213d1da
Xiao, Xinyu <xiaox@rose-hulman.edu>
2015-07-21 10:57:29 -0400

    logo
    Former-commit-id: 254547f17fe77bddc7306f9f0984b3d77cf0fdb8

--- iOS/Bakkle/Bakkle/LoginView.swift
+++ iOS/Bakkle/Bakkle/LoginView.swift
@@ -25,14 +25,15 @@ class LoginView: UIViewController, FBLoginViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        logoImageView.image = UIImage(named: "Goodwill Logo-White.png")!
-        logo.image = UIImage(named: "Goodwill Logo-White.png")!
+        if Bakkle.sharedInstance.flavor == 2 {
+            logoImageView.image = UIImage(named: "Goodwill Logo-White.png")!
+        }
         
         self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends", "publish_actions"]
         
         // add the image, making the login view looks like the launch screen when user already logged in
         setBackgroundImg()
-//        setLogoImg()
+        setLogoImg()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -56,6 +57,22 @@ class LoginView: UIViewController, FBLoginViewDelegate {
         background = UIImageView(image: UIImage(named: "SplashScreen-bkg.png"))
         background.frame = CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height)
         self.view.addSubview(background)
+    }
+    
+    // create the logo image, which is the same as the launch screen logo
+    func setLogoImg(){
+        if Bakkle.sharedInstance.flavor == 2 {
+            logo = UIImageView(image: UIImage(named: "Goodwill Logo-White.png"))
+        }else{
+            logo = UIImageView(image: UIImage(named: "logo-white-design-clear.png"))
+        }
+        background.addSubview(logo)
+        
+        logo.setTranslatesAutoresizingMaskIntoConstraints(false)
+        logo.addConstraint(NSLayoutConstraint(item: logo, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: logo, attribute: NSLayoutAttribute.Width, multiplier: 25.0/62.0, constant: 0.0))
+        background.addConstraint(NSLayoutConstraint(item: logo, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: background, attribute: NSLayoutAttribute.CenterX, multiplier: 1.0, constant: 0.0))
+        background.addConstraint(NSLayoutConstraint(item: logo, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: background, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 49.0))
+        background.addConstraint(NSLayoutConstraint(item: logo, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: background, attribute: NSLayoutAttribute.Left, multiplier: 1.0, constant: 36.0))
     }
     
     func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
