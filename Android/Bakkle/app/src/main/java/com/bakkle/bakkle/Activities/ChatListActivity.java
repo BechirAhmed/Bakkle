1714e2af3d0b0b037e9afec3c54e074ea13fe8a3
Vansh Gandhi <vansh.gandhi@gmail.com>
2015-08-10 16:42:15 -0700

    Added ability to click on item in sellers garage to get the chat list
    Former-commit-id: 0533cef2e358915c5ced1447034373d6304ae205

--- Android/Bakkle/app/src/main/java/com/bakkle/bakkle/Activities/ChatListActivity.java
+++ Android/Bakkle/app/src/main/java/com/bakkle/bakkle/Activities/ChatListActivity.java
@@ -4,6 +4,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
@@ -15,16 +16,26 @@ import com.bakkle.bakkle.Adapters.ChatListAdapter;
 import com.bakkle.bakkle.Helpers.BuyerInfo;
 import com.bakkle.bakkle.Helpers.ChatCalls;
 import com.bakkle.bakkle.R;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.WebSocket;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
-public class ChatListActivity extends ListActivity {
+public class ChatListActivity extends ListActivity
+{
 
     ArrayList<BuyerInfo> buyerInfos = null;
     JsonObject json;
     SharedPreferences preferences;
+    ChatCalls chatCalls;
+    ChatListAdapter chatListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +43,88 @@ public class ChatListActivity extends ListActivity {
         setContentView(R.layout.activity_chat_list);
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        new Thread(new Runnable()
+        Handler h = new Handler(getMainLooper());
+        Runnable r = new Runnable()
         {
             @Override
             public void run()
             {
+                Log.v("testing this", "test 123 123");
+            }
+        };
+
+        buyerInfos = new ArrayList<>();
+        chatListAdapter = new ChatListAdapter(this, buyerInfos);
+        setListAdapter(chatListAdapter);
 
 
+        preferences = PreferenceManager.getDefaultSharedPreferences(ChatListActivity.this);
+        chatCalls = new ChatCalls(preferences.getString("uuid", ""), preferences.getString("sellerPk", ""), preferences.getString("auth_token", ""), h, r, new WebSocketCallBack());
+        chatCalls.connect();
+//        chatCalls.test();
+//        Log.v("the url is:", "ws://app.bakkle.com/ws/" + "?uuid=" + preferences.getString("uuid", "") + "&userId=" + preferences.getString("sellerPk", ""));
+//        chatCalls.getChatList();
+
+    }
+
+
+    private class WebSocketCallBack implements AsyncHttpClient.WebSocketConnectCallback
+    {
+
+        @Override
+        public void onCompleted(Exception ex, WebSocket webSocket)
+        {
+            JSONObject json = new JSONObject();
+            try {
+                json.put("method", "chat_getChatIds");
+                json.put("itemId", "14");
+                json.put("uuid", "E7F742EB-67EE-4738-ABEC-F0A3B62B45EB");
+                json.put("auth_token", "f02dfb77e9615ae630753b37637abb31_10");
+                Log.v("the json is ", json.toString());
+            }
+            catch (Exception e) {
+            }
+
+            webSocket.send(json.toString());
+            webSocket.setStringCallback(new WebSocket.StringCallback()
+            {
+                @Override
+                public void onStringAvailable(String s)
+                {
+                    populateList(s);
+                }
+            });
+
+        }
+    }
+
+    public void populateList(String result)
+    {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(result);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        int status = jsonObject.get("success").getAsInt();
+        if(status != 1 || jsonObject.has("message"))
+            return;
+        Log.v("testing", result);
+        JsonArray chatArray = jsonObject.get("chats").getAsJsonArray();
+        for(JsonElement element : chatArray)
+        {
+            JsonObject temp = element.getAsJsonObject();
+            JsonObject buyer = temp.getAsJsonObject("buyer");
+            String url = "https://graph.facebook.com/"+ buyer.get("facebook_id").getAsString() +"/picture?width=142&height=142";
+            buyerInfos.add(new BuyerInfo(buyer.get("display_name").getAsString(), url));
+        }
+        runOnUiThread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                chatListAdapter.notifyDataSetChanged();
             }
         });
 
-        preferences = PreferenceManager.getDefaultSharedPreferences(ChatListActivity.this);
-        ChatCalls chatCalls;
-        chatCalls = new ChatCalls(preferences.getString("uuid", ""), preferences.getString("sellerPk", ""), preferences.getString("auth_token", ""));
-        chatCalls.connect();
-        Log.v("the url is:", "ws://app.bakkle.com/ws/" + "?uuid=" + preferences.getString("uuid", "") + "&userId=" + preferences.getString("sellerPk", ""));
-        chatCalls.getChatList();
-
-
-        buyerInfos = new ArrayList<>();
-        buyerInfos.add(new BuyerInfo("Vansh", "http://i.imgur.com/RotoKyk.jpg"));
-        buyerInfos.add(new BuyerInfo("John", "http://i.imgur.com/RotoKyk.jpg"));
-        setListAdapter(new ChatListAdapter(ChatListActivity.this, buyerInfos));
-
-
+        //JsonObject buyerObject = chatArray.get(0).getAsJsonObject().get("buyer").getAsJsonObject()
     }
 
     @Override
