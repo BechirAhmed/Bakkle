0193c3a0b55c617d125eb5a816a62c5e0fb3766a
Vansh Gandhi <vansh.gandhi@gmail.com>
2015-10-03 18:23:20 -0700

    Removed unused blurring library (might be used again in future). Alpha build 2
    Former-commit-id: 7087caad2869a9f3b7ae880a32b0e0ac008e1151

--- Android/Bakkle/StackBlur/build/generated/source/r/release/com/enrique/stackblur/R.java
+++ Android/Bakkle/StackBlur/build/generated/source/r/release/com/enrique/stackblur/R.java
@@ -14,14 +14,14 @@ public final class R {
         public static int activity_horizontal_margin=0x7f040000;
         public static int activity_vertical_margin=0x7f040001;
     }
-    public static final class drawable {
-        public static int ic_launcher=0x7f020000;
-    }
     public static final class id {
         public static int action_settings=0x7f080000;
     }
     public static final class menu {
         public static int main=0x7f070000;
+    }
+    public static final class mipmap {
+        public static int ic_launcher=0x7f020000;
     }
     public static final class raw {
         public static int blur=0x7f030000;
